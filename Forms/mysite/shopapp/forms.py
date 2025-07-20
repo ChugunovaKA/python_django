c332a4a9a95346777f45d28256dee3269b994750
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-20 17:33:38 +0400

    Модуль 6

--- Forms/mysite/shopapp/forms.py
+++ Forms/mysite/shopapp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Product
+from .models import Product, Order  # Импортируем обе модели
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'discount']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
