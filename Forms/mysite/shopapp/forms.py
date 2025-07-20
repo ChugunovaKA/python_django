27b54afde56396dc06efc30688e911e9ff38d24f
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-20 17:17:10 +0400

    Модуль 6

--- Forms/mysite/shopapp/forms.py
+++ Forms/mysite/shopapp/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price', 'description']  # Замените поля на реально используемые в вашей модели Product
+        fields = ['name', 'description', 'price', 'discount']
