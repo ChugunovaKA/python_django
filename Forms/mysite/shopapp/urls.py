25f4a481cb24f23c147162e38c2738825a423a65
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-20 17:07:59 +0400

    Модуль 5

--- Forms/mysite/shopapp/urls.py
+++ Forms/mysite/shopapp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-
-from .views import shop_index, groups_list, products_list, orders_list
-
-app_name = "shopapp"
+from .views import (
+    shop_index,
+    groups_list,
+    products_list,
+    orders_list,
+    create_product,
+)
 
 urlpatterns = [
-    path("", shop_index, name="index"),
-    path("groups/", groups_list, name="groups_list"),
-    path("products/", products_list, name="products_list"),
-    path("orders/", orders_list, name="orders_list"),
+    path('', shop_index, name='shop_index'),
+    path('groups/', groups_list, name='groups_list'),
+    path('products/', products_list, name='products_list'),
+    path('orders/', orders_list, name='orders_list'),
+    path('product/create/', create_product, name='create_product'),  # Новый маршрут для создания продукта
 ]
