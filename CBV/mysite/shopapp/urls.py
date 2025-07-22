4f732df176d347b3ad995ad99f7da765fd719b57
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-22 12:05:03 +0400

    Модуль 8

--- CBV/mysite/shopapp/urls.py
+++ CBV/mysite/shopapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, ProductListView, ProductDetailView, orders_list
+from .views import (
+    shop_index,
+    groups_list,
+    ProductListView,
+    ProductDetailView,
+    ProductUpdateView,
+    orders_list,
+)
 
 app_name = "shopapp"
 
@@ -9,5 +16,6 @@ urlpatterns = [
     path("groups/", groups_list, name="groups_list"),
     path("products/", ProductListView.as_view(), name="products_list"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("products/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
     path("orders/", orders_list, name="orders_list"),
 ]
