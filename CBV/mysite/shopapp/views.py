bdca7ebce184ba1ac262398db1d9d80649de9919
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-22 10:51:45 +0400

    Модуль 8

---
+++ CBV/mysite/shopapp/views.py
@@ -0,0 +1,44 @@
+from timeit import default_timer
+
+from django.contrib.auth.models import Group
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Product, Order
+
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shopapp/products-list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.filter(archived=False)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all(),
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
