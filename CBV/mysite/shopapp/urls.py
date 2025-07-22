dcb5ab74194d244c849ad72ba6efd63226fe8e11
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-22 12:06:39 +0400

    Модуль 8

--- CBV/mysite/shopapp/urls.py
+++ CBV/mysite/shopapp/urls.py
@@ -6,6 +6,7 @@ from .views import (
     ProductListView,
     ProductDetailView,
     ProductUpdateView,
+    product_archive,  # импортируем функцию архивации
     orders_list,
 )
 
@@ -17,5 +18,6 @@ urlpatterns = [
     path("products/", ProductListView.as_view(), name="products_list"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("products/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("products/archive/<int:pk>/", product_archive, name="product_archive"),  # маршрут для архивации
     path("orders/", orders_list, name="orders_list"),
 ]
