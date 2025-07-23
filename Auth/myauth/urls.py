5b00aa5e5b6f46c52b5d6f8d2e260b6ace17f0b1
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-23 10:51:48 +0400

    Модуль 9

--- Auth/myauth/urls.py
+++ Auth/myauth/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from .views import MyLogoutView
+from .views import MyLogoutView, SetCookieView, GetCookieView
 
 urlpatterns = [
     path('login/', LoginView.as_view(
         template_name='myauth/login.html',
         redirect_authenticated_user=True
     ), name='login'),
+
     path('logout/', MyLogoutView.as_view(), name='logout'),
+
+    path('set-cookie/', SetCookieView.as_view(), name='set_cookie'),
+    path('get-cookie/', GetCookieView.as_view(), name='get_cookie'),
 ]
--- Auth/myauth/views.py
+++ Auth/myauth/views.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.views import LogoutView
-from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.views import View
+
 
 class MyLogoutView(LogoutView):
-    next_page = '/'
+    next_page = '/'  # страница для переадресации после выхода
+
+
+class SetCookieView(View):
+    def get(self, request):
+        response = HttpResponse('Cookie установлена')
+        response.set_cookie('mycookie', 'value123', max_age=3600)  # куки на 1 час
+        return response
+
+
+class GetCookieView(View):
+    def get(self, request):
+        value = request.COOKIES.get('mycookie', 'Значение по умолчанию')
+        return HttpResponse(f'Значение cookie: {value}')
