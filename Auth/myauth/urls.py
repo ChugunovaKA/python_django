aca99933001eb60985281a43b0c51487cc45ed51
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-23 10:49:00 +0400

    Модуль 9

--- Auth/myauth/urls.py
+++ Auth/myauth/urls.py
@@ -3,4 +3,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    path('login/', LoginView.as_view(
+        template_name='myauth/login.html',      # имя шаблона
+        redirect_authenticated_user=True        # если пользователь уже аутентифицирован — редирект
+    ), name='login'),
+    # логаут добавим позже, вместе с остальными views
 ]
