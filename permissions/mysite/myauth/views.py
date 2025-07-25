e9d371126fceef49c389d970fa472b995fd7afc2
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 16:39:55 +0400

    Модуль 10

--- permissions/mysite/myauth/views.py
+++ permissions/mysite/myauth/views.py
@@ -1,4 +1,17 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
-class AboutMeView(TemplateView):
-    template_name = "myauth/about_me.html"
+from .models import Profile
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'myauth/register.html'  # Создай этот шаблон, если ещё нет
+    success_url = reverse_lazy('login')    # После регистрации переходит на страницу входа
+
+    def form_valid(self, form):
+        # Сохраняем пользователя, вызов родительского метода вернёт response
+        response = super().form_valid(form)
+        # Создаём связанный профиль для нового пользователя
+        Profile.objects.create(user=self.object)
+        return response
