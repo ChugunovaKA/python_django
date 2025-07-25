0a99b683b9763808e898faee4c62a07e6994b0f2
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 14:08:17 +0400

    Модуль 10

--- permissions/mysite/shopapp/urls.py
+++ permissions/mysite/shopapp/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views import (
     ShopIndexView,
     ProductsListView,
-    OrdersListView,
-    OrderDetailView,
     ProductCreateView,
     ProductUpdateView,
     ProductDeleteView,
@@ -16,9 +14,6 @@ urlpatterns = [
     path("", ShopIndexView.as_view(), name="index"),
     path("products/", ProductsListView.as_view(), name="products_list"),
     path("products/create/", ProductCreateView.as_view(), name="product_create"),
-    # Удалена строка с product_details, т.к. ProductDetailsView удалён
     path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
     path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name="product_delete"),
-    path("orders/", OrdersListView.as_view(), name="orders_list"),
-    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order_details"),
 ]
