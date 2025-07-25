9b1c8ecdd0e2e191afccb3d46265b7531f265e83
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 10:25:30 +0400

    Модуль 10

--- permissions/mysite/shopapp/views.py
+++ permissions/mysite/shopapp/views.py
@@ -7,11 +7,9 @@ from .models import Product
 class ProductCreateView(PermissionRequiredMixin, CreateView):
     model = Product
     fields = ("name", "price", "description", "discount")
-    permission_required = "shopapp.add_product"  # доступ к созданию продукта имеет пользователь с этим правом
+    permission_required = "shopapp.add_product"
     success_url = reverse_lazy("shopapp:products_list")
 
-    # (опционально) если хотите задать страницу логина явно, если пользователь не залогинен:
-    # login_url = reverse_lazy("accounts:login")
-
-    # (опционально) чтобы отдавать 403 вместо редиректа:
-    # raise_exception = True
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
