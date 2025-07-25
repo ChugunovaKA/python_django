b07c161e5558ca6ef8015fec05e4c6c55d9626c4
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 10:07:32 +0400

    Модуль 10

--- permissions/mysite/myauth/urls.py
+++ permissions/mysite/myauth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views  # импорт стандартных views
 
 from .views import (
     AboutMeView,
@@ -9,4 +9,15 @@ app_name = "myauth"
 
 urlpatterns = [
     path("about-me/", AboutMeView.as_view(), name="about-me"),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="myauth/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(next_page="/accounts/login/"),  # при выходе редиректим на логин
+        name="logout",
+    ),
 ]
