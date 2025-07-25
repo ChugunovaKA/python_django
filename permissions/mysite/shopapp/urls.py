a3c4a3a998f1e8c54264e1e9eb694ba3f9698836
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 17:21:38 +0400

    Модуль 10

--- permissions/mysite/shopapp/urls.py
+++ permissions/mysite/shopapp/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-
-from .views import (
-    ShopIndexView,
-    ProductsListView,
-    ProductCreateView,
-    # ProductDeleteView временно убран
-)
+from .views import ProductUpdateView, ProductsListView, ProductCreateView
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", ShopIndexView.as_view(), name="index"),
     path("products/", ProductsListView.as_view(), name="products_list"),
     path("products/create/", ProductCreateView.as_view(), name="product_create"),
-    # path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name="product_delete"),  # временно закомментировано
+    path("products/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_update"),
 ]
