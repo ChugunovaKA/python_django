fb8ffc8bc59b63e2811839dce0747e068e4512e8
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 14:11:43 +0400

    Модуль 10

--- permissions/mysite/shopapp/urls.py
+++ permissions/mysite/shopapp/urls.py
@@ -4,7 +4,6 @@ from .views import (
     ShopIndexView,
     ProductsListView,
     ProductCreateView,
-    ProductUpdateView,
     ProductDeleteView,
 )
 
@@ -14,6 +13,7 @@ urlpatterns = [
     path("", ShopIndexView.as_view(), name="index"),
     path("products/", ProductsListView.as_view(), name="products_list"),
     path("products/create/", ProductCreateView.as_view(), name="product_create"),
-    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    # Обновление продукта временно отключено
+    # path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
     path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name="product_delete"),
 ]
