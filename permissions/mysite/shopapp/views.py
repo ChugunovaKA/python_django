202bcb8c952dd9aa983a4d4b0c481d33bbcac593
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 14:23:29 +0400

    Модуль 10

--- permissions/mysite/shopapp/views.py
+++ permissions/mysite/shopapp/views.py
@@ -1,28 +1,16 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 from .models import Product
 
-# Главная страница магазина (обязательный класс, чтобы устранить ошибку импорта ShopIndexView)
-class ShopIndexView(TemplateView):
-    template_name = "shopapp/index.html"
-
-
-# Создание продукта с проверкой разрешения
 class ProductCreateView(PermissionRequiredMixin, CreateView):
     model = Product
     fields = ("name", "price", "description", "discount")
-    permission_required = "shopapp.add_product"
+    template_name = "shopapp/product_create.html"  # проверьте путь к шаблону
     success_url = reverse_lazy("shopapp:products_list")
+    permission_required = "shopapp.add_product"
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
-
-
-# Класс списка продуктов
-class ProductsListView(ListView):
-    model = Product
-    template_name = "shopapp/products_list.html"
-    context_object_name = "products"
