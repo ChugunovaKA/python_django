11eef86171476dd89ce8c969dab95914d291ac9e
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-25 11:10:06 +0400

    Модуль 10

--- permissions/mysite/myauth/models.py
+++ permissions/mysite/myauth/models.py
@@ -1,9 +1,15 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    bio = models.TextField(blank=True, default="", verbose_name="Биография")
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    # Добавьте сюда любые дополнительные поля, например:
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"Profile of {self.user.username}"
