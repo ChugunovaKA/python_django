0b3ea8ccbb8569875be67a2ab16ffffb8400470e
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-26 10:52:57 +0400

    Модуль 10

--- permissions/mysite/mysite/settings.py
+++ permissions/mysite/mysite/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
-
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Путь к общей папке шаблонов
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
