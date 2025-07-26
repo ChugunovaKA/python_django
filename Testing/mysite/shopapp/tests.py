607286bd992acb582a22e1c703e5e3ce7e5bc5bb
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-26 17:52:06 +0400

    Модуль 11

--- Testing/mysite/shopapp/tests.py
+++ Testing/mysite/shopapp/tests.py
@@ -59,3 +59,76 @@ class OrderDetailViewTestCase(TestCase):
 
         # Проверяем, что в контексте тот же заказ
         self.assertEqual(response.context['order'].pk, self.order.pk)
+
+
+class OrdersExportViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Обычный пользователь без прав staff
+        cls.user = User.objects.create_user(username='regularuser', password='userpass')
+
+        # Staff пользователь с правами
+        cls.staff_user = User.objects.create_user(username='staffuser', password='staffpass', is_staff=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
+        cls.staff_user.delete()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.client = Client()
+
+        # Создаем тестовый продукт
+        self.product = Product.objects.create(
+            name='Test Product',
+            description='Test Description',
+            price=9.99,
+            discount=0,
+            archived=False,
+        )
+
+        # Создаем заказ, связанный со staff пользователем
+        self.order = Order.objects.create(
+            delivery_address='Test Address',
+            promocode='PROMO',
+            user=self.staff_user,
+        )
+        self.order.products.add(self.product)
+
+    def tearDown(self):
+        self.order.delete()
+        self.product.delete()
+
+    def test_access_requires_staff(self):
+        # Без авторизации - доступ запрещён (обычно 302 редирект на логин)
+        response = self.client.get(reverse('shopapp:orders_export'))
+        self.assertNotEqual(response.status_code, 200)
+
+        # Залогинен обычный пользователь - доступ запрещён
+        self.client.login(username='regularuser', password='userpass')
+        response = self.client.get(reverse('shopapp:orders_export'))
+        self.assertNotEqual(response.status_code, 200)
+        self.client.logout()
+
+        # Залогинен staff-пользователь - доступ разрешён
+        self.client.login(username='staffuser', password='staffpass')
+        response = self.client.get(reverse('shopapp:orders_export'))
+        self.assertEqual(response.status_code, 200)
+
+        # Проверяем JSON-структуру и данные
+        data = response.json()
+        self.assertIn("orders", data)
+        self.assertTrue(isinstance(data["orders"], list))
+        self.assertGreaterEqual(len(data["orders"]), 1)
+
+        # Проверяем поля первого заказа
+        order_data = data["orders"][0]
+        self.assertEqual(order_data["id"], self.order.id)
+        self.assertEqual(order_data["delivery_address"], self.order.delivery_address)
+        self.assertEqual(order_data["promocode"], self.order.promocode)
+        self.assertEqual(order_data["user_id"], self.staff_user.id)
+        self.assertIn(self.product.id, order_data["product_ids"])
+
+        self.client.logout()
