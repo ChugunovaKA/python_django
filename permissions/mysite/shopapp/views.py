9844189f79516d40ad5a5599a384551006df28df
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-07-26 09:43:56 +0400

    Модуль 10

--- permissions/mysite/shopapp/views.py
+++ permissions/mysite/shopapp/views.py
@@ -1,17 +1,16 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import UpdateView
+from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from .models import Product
 
-class ProductUpdateView(UserPassesTestMixin, UpdateView):
+class ProductsListView(ListView):
     model = Product
-    fields = ('name', 'price', 'description', 'discount')  # поля редактирования
-    template_name = 'shopapp/product_update.html'          # путь к шаблону
-    success_url = reverse_lazy('shopapp:products_list')    # куда переходит после сохранения
+    template_name = 'shopapp/products_list.html'  # проверьте наличие этого шаблона
+    context_object_name = 'products'
 
-    def test_func(self):
-        product = self.get_object()
-        user = self.request.user
-        # Суперпользователь всегда может редактировать
-        # Остальные — только если есть разрешение и автор продукта
-        return user.is_superuser or (user.has_perm('shopapp.change_product') and product.created_by == user)
+class ProductCreateView(PermissionRequiredMixin, CreateView):
+    model = Product
+    fields = ('name', 'price', 'description', 'discount')
+    template_name = 'shopapp/product_create.html'  # проверьте наличие шаблона
+    permission_required = 'shopapp.add_product'
+    success_url = reverse_lazy('shopapp:products_list')
