dd5c28f6c9daaff4f52f4d0ebdda95063a05d21f
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-01 19:26:36 +0400

    Модуль 13

--- i18n_l10n/mysite/shopapp/forms.py
+++ i18n_l10n/mysite/shopapp/forms.py
@@ -1,13 +1,29 @@
 from django import forms
-
 from shopapp.models import Product
+# Импортируйте свою модель для изображений, если есть
+# from shopapp.models import ProductImage
 
 
 class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = "name", "price", "description", "discount", "preview"
-
     images = forms.ImageField(
         widget=forms.ClearableFileInput(attrs={"multiple": True}),
+        required=False  # чтобы не было ошибки, если не загрузить файлы
     )
+
+    class Meta:
+        model = Product
+        fields = ("name", "price", "description", "discount", "preview")
+
+    def save(self, commit=True):
+        # Сначала сохраняем основную модель Product
+        instance = super().save(commit=commit)
+
+        # Обрабатываем загруженные изображения (если они есть)
+        images = self.files.getlist('images')
+        for image in images:
+            # Пример сохранения связанных изображений
+            # Предположим, есть модель ProductImage с ForeignKey к Product
+            # ProductImage.objects.create(product=instance, image=image)
+            pass
+
+        return instance
