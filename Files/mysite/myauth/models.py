45fd264264756f47dac6e41a340c9c798057816f
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-01 17:30:18 +0400

    Модуль 12

--- Files/mysite/myauth/models.py
+++ Files/mysite/myauth/models.py
@@ -3,13 +3,13 @@ from django.db import models
 import os
 
 def user_avatar_path(instance, filename):
-    # Функция для формирования пути загрузки аватара с использованием pk пользователя
+    # Формирует путь загрузки аватара: media/avatars/<user_pk>/avatar.<ext>
     ext = filename.split('.')[-1]  # расширение файла
     filename = f'avatar.{ext}'  # фиксированное имя файла avatar с оригинальным расширением
-    return os.path.join('avatars', str(instance.user.pk), filename)  # путь media/avatars/<user_pk>/avatar.ext
+    return os.path.join('avatars', str(instance.user.pk), filename)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(max_length=500, blank=True)
     agreement_accepted = models.BooleanField(default=False)
     avatar = models.ImageField(upload_to=user_avatar_path, blank=True, null=True)
