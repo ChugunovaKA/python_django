033f3dd72afdd95e827850e5cd41425a01d5527d
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-05 10:19:58 +0400

    Модуль 16

--- queries/mysite/blogapp/views.py
+++ queries/mysite/blogapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
 
-# Create your views here.
+class ArticlesListView(ListView):
+    model = Article
+    template_name = 'blogapp/article_list.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        return (
+            Article.objects
+            .select_related('author', 'category')
+            .prefetch_related('tags')
+            .defer('content')
+            .all()
+        )
