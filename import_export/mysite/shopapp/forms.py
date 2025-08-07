bd53de732c18a2c7f23ffdfa770ebe5ab8d566e1
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-07 11:22:09 +0400

    Модуль 18

--- import_export/mysite/shopapp/forms.py
+++ import_export/mysite/shopapp/forms.py
@@ -7,7 +7,7 @@ class ProductForm(forms.ModelForm):
         fields = "name", "price", "description", "discount", "preview"
 
     images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={"multiple": True}),
+        widget=forms.ClearableFileInput(),  # убран attrs={"multiple": True}
     )
 
 
