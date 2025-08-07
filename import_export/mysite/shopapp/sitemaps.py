fb65a2062d9582aecdf9f1c68faaf523b3cd0d74
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-07 12:12:57 +0400

    Модуль 18

--- import_export/mysite/shopapp/sitemaps.py
+++ import_export/mysite/shopapp/sitemaps.py
@@ -1,4 +1,5 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
 from .models import Product
 
 class ShopSitemap(Sitemap):
@@ -6,12 +7,14 @@ class ShopSitemap(Sitemap):
     priority = 0.9
 
     def items(self):
-        # Возвращаем только неархивированные продукты, если применимо
+        # Возвращаем только неархивированные товары
         return Product.objects.filter(archived=False)
 
     def lastmod(self, obj):
-        # Используем 'created_at', так как 'updated_at' отсутствует
+        # Возвращаем дату создания, так как updated_at отсутствует
         return obj.created_at
 
     def location(self, obj):
+        # Возвращаем URL товара через get_absolute_url()
+        # Можно заменить на reverse(), если требуется
         return obj.get_absolute_url()
