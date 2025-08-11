9534615c27c3e99bb48e0f5ae704a00642653515
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-11 12:35:43 +0400

    Модуль 19

--- Django_Cache/mysite/shopapp/urls.py
+++ Django_Cache/mysite/shopapp/urls.py
@@ -12,6 +12,8 @@ from .views import (
     ProductDeleteView,
     ProductsDataExportView,
     ProductViewSet,
+    UserOrdersListView,        # добавлено
+    UserOrdersExportView,      # добавлено
 )
 
 app_name = "shopapp"
@@ -22,12 +24,26 @@ routers.register("products", ProductViewSet)
 urlpatterns = [
     path("", ShopIndexView.as_view(), name="index"),
     path("api/", include(routers.urls)),
+
     path("products/", ProductsListView.as_view(), name="products_list"),
     path("products/export/", ProductsDataExportView.as_view(), name="products-export"),
     path("products/create/", ProductCreateView.as_view(), name="product_create"),
     path("products/<int:pk>/", ProductDetailsView.as_view(), name="product_details"),
     path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
     path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name="product_delete"),
+
     path("orders/", OrdersListView.as_view(), name="orders_list"),
     path("orders/<int:pk>/", OrderDetailView.as_view(), name="order_details"),
+
+    # Добавляем маршруты для страницы заказов конкретного пользователя и экспорта заказов
+    path(
+        "users/<int:user_id>/orders/",
+        UserOrdersListView.as_view(),
+        name="user_orders_list",
+    ),
+    path(
+        "users/<int:user_id>/orders/export/",
+        UserOrdersExportView.as_view(),
+        name="user_orders_export",
+    ),
 ]
