6716b7daed941e3323c5f1966a867af34a2ccd7e
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-11 12:48:51 +0400

    Модуль 19

--- Django_Cache/mysite/shopapp/forms.py
+++ Django_Cache/mysite/shopapp/forms.py
@@ -2,16 +2,15 @@ from django import forms
 
 from shopapp.models import Product
 
-
 class ProductForm(forms.ModelForm):
+    images = forms.ImageField(
+        widget=forms.FileInput(attrs={"multiple": True}),  # изменено с ClearableFileInput на FileInput
+        required=False,
+    )
+
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount", "preview"
-
-    images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={"multiple": True}),
-    )
-
 
 class CSVImportForm(forms.Form):
     csv_file = forms.FileField()
