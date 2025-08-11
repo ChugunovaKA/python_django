befe69b55c7c0168982098515bd9adeafd32f6ea
kristina-chugunova <kristina.gureva.90@mail.ru>
2025-08-11 12:50:24 +0400

    Модуль 19

--- Django_Cache/mysite/shopapp/forms.py
+++ Django_Cache/mysite/shopapp/forms.py
@@ -1,16 +1,55 @@
 from django import forms
 
-from shopapp.models import Product
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    widget = MultipleFileInput
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, data):
+        # Ожидаем, что data — это список файлов
+        if not data:
+            return None
+        elif isinstance(data, list):
+            return data
+        else:
+            return [data]
+
+    def validate(self, data):
+        # Если список файлов с несколькими элементами
+        if isinstance(data, list):
+            for file in data:
+                super().validate(file)
+        else:
+            super().validate(data)
+
+    def clean(self, data, initial=None):
+        if not data:
+            return None
+        if not isinstance(data, list):
+            data = [data]
+        cleaned_data = []
+        errors = []
+        for file in data:
+            try:
+                cleaned_file = super().clean(file, initial)
+                cleaned_data.append(cleaned_file)
+            except forms.ValidationError as e:
+                errors.append(e)
+        if errors:
+            raise forms.ValidationError(errors)
+        return cleaned_data
+
 
 class ProductForm(forms.ModelForm):
-    images = forms.ImageField(
-        widget=forms.FileInput(attrs={"multiple": True}),  # изменено с ClearableFileInput на FileInput
+    images = MultipleFileField(
         required=False,
+        widget=MultipleFileInput(attrs={"multiple": True}),
     )
 
     class Meta:
         model = Product
-        fields = "name", "price", "description", "discount", "preview"
-
-class CSVImportForm(forms.Form):
-    csv_file = forms.FileField()
+        fields = ("name", "price", "description", "discount", "preview",)
