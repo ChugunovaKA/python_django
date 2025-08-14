7f54a9de8b74111204d368925fe948a683880ef2
ChugunovaKA <kristina.gureva.90@mail.ru>
2025-08-14 14:19:34 +0400

    Delete M_01_LinuxIntro directory

--- M_01_LinuxIntro/shopapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, products_list, orders_list  # импортируем все три view-функции
-
-urlpatterns = [
-    path('', index, name='index'),  # главная страница
-    path('products/', products_list, name='products_list'),  # страница со списком продуктов
-    path('orders/', orders_list, name='orders_list'),  # страница со списком заказов
-]
